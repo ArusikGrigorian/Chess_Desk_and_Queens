f29b2815155370a833af2711e8514ce3d761129b
Arusik <58630658+ArusikGrigorian@users.noreply.github.com>
2020-02-18 14:33:48 +0400

    Add files via upload

---
+++ ChessDesk&Queens.js
@@ -0,0 +1,18 @@
+function chessdesk(num){
+var k="";
+  var l ="";
+for(var i=0; i<num; i++){
+  k="";
+  for(var j=0; j<num; j++){
+    if(i===0 && j===0 || j===num-i/2 || j===(num-i/2)-num/2){
+      k+=" G ";
+    }
+    else{
+    k+=" . ";
+    }
+  }
+  l += k+"\n ";
+}
+  return l;
+}
+console.log(chessdesk(5)); // for all odd numbers
